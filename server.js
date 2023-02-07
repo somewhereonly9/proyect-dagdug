1501f2e0e82f85f01727dfb573099da2077741a1
somewhereonly9 <somewhereonlyou@gmail.com>
2023-02-07 06:16:26 -0600

    Cambio de mensaje en pantalla

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1> Node its ON in port 3000 using HTML'))
 
-app.listen(8080);
-console.log('Server is UP on port 8080');
+app.listen(80);
+console.log('Server is UP on port 80');
