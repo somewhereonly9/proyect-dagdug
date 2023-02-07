1a7a2d9eb7ae1f4342654ea845dd75742a2d66a0
somewhereonly9 <somewhereonlyou@gmail.com>
2023-02-06 19:24:09 -0600

    update text

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('El gordo esta panzon'))
+app.get('/', (req, res) => res.send('<h1> Node its ON in port 3000 using HTML'))
 
 app.listen(3000);
 console.log('Server is UP');
